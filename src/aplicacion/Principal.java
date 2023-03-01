d88c88f7e9476cf0a4ae8925ea7f664c7cd382f7
AlejandroLilloRodriguez <alejandro.lillorodriguez@usp.ceu.es>
2023-03-01 09:32:43 +0100

    Ejercicio 14 terminado

--- src/aplicacion/Principal.java
+++ src/aplicacion/Principal.java
@@ -1,2 +1,46 @@
-package aplicacion;public class Principal {
+package aplicacion;
+import dominio.Matematicas;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Principal {
+    public static void main(String[] args) {
+        int[]listaInt={1,2,3,4,5,6,7,8,9,10};
+        List<String> lista=new ArrayList<>();
+        lista.add("Hola");
+        lista.add("Mundo");
+        lista.add("Alber");
+        lista.add("JAVA");
+        lista.add("ProgrAmacion");
+        lista.add("FuNcioNal");
+        lista.add("AleJandro");
+        lista.add("Almeria");
+        List<Integer> lista2=new ArrayList<>();
+        lista2.add(1);
+        lista2.add(2);
+        lista2.add(3);
+        lista2.add(4);
+        lista2.add(5);
+        lista2.add(6);
+        lista2.add(7);
+        lista2.add(8);
+        lista2.add(9);
+        lista2.add(10);
+
+        double media=Matematicas.media(listaInt);
+        double alcuadrado= Matematicas.alcuadrado(listaInt);
+        double varianza=(alcuadrado/listaInt.length)-media*media;
+        double desviaciontipica=Math.sqrt(varianza);
+        System.out.println("Los cuadrados de los elementos de la lista son: "+Matematicas.cuadrados(listaInt));
+        System.out.println("La suma de los elementos de la lista es: "+Matematicas.suma(listaInt));
+        System.out.println("La suma de los elementos pares de la lista es: "+Matematicas.sumaPares(listaInt));
+        System.out.println("La lista de los elementos pares de la lista es: "+Matematicas.listaPares(listaInt));
+        System.out.println("La media de los elementos de la lista es: "+media);
+        System.out.println("La desviacion tipica de los elementos de la lista es: "+desviaciontipica);
+        System.out.println("El elemento menor de la lista es: "+Matematicas.menor(listaInt));
+        System.out.println("La lista de elementos en minusculas es: "+Matematicas.aMinusculas(lista));
+        System.out.println("La concatenacion de los elementos de la lista es: "+Matematicas.concatenarCadenas(lista));
+        System.out.println("La lista de elementos en minusculas es: "+Matematicas.transformarAMinusculas(lista));
+        System.out.println("La lista de elementos que no empiezan por A es: "+Matematicas.noEmpiezaPorA(lista));
+    }
 }
--- src/dominio/Matematicas.java
+++ src/dominio/Matematicas.java
@@ -1,2 +1,44 @@
-package dominio;public class Matematicas {
+package dominio;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.List;
+public class Matematicas {
+    public static ArrayList cuadrados(int[]listaInt){
+        return Stream.iterate(0,x->x+1).limit(listaInt.length).map(x->listaInt[x]*listaInt[x]).collect(Collectors.toCollection(ArrayList::new));
+    }
+    public static int suma(int [] listaInt){
+        return Stream.iterate(0,x->x+1).limit(listaInt.length).map(x->listaInt[x]).reduce(0,(x,y)->x+y);
+    }
+    public static int sumaPares(int[] listaInt){
+        return Stream.iterate(0,x->x+1).limit(listaInt.length).map(x->listaInt[x]).filter(x->x%2==0).reduce(0,(x,y)->x+y);
+    }
+    public static ArrayList listaPares(int[]listaInt){
+        return Stream.iterate(0,x->x+1).limit(listaInt.length).map(x->listaInt[x]).filter(x->x%2==0).collect(Collectors.toCollection(ArrayList::new));
+    }
+    public static int media (int[]listaInt){
+        return Stream.iterate(0,i->i+1).limit(listaInt.length).map(i->listaInt[i]).reduce(0,(x,y)->(x+y))/listaInt.length;
+    }
+    public static double alcuadrado(int[]listaInt){
+        return Stream.iterate(0,x->x+1).limit(listaInt.length).map(n->listaInt[n]).reduce(0,(x,y)->x+y*y);
+    }
+    public static int menor(int[]listaInt){
+        return Stream.iterate(0,x->x+1).limit(listaInt.length).map(x->listaInt[x]).min(Integer::compare).get();
+    }
+    public static List<String> aMinusculas(List<String> lista){
+        return Stream.iterate(0,x->x+1).limit(lista.size()).map(x->lista.get(x).toLowerCase()).collect(Collectors.toList());
+    }
+    public static String concatenarCadenas(List<String> lista){
+        return Stream.iterate(0,x->x+1).limit(lista.size()).map(x->lista.get(x)).reduce("",(x,y)->x+y);
+    }
+    public static List<String> transformarAMinusculas(List<String> lista){
+        return Stream.iterate(0,x->x+1).limit(lista.size()).map(x->lista.get(x).toLowerCase()).collect(Collectors.toCollection(ArrayList:: new));
+    }
+    public static List<String> noEmpiezaPorA(List<String> lista){
+        return Stream.iterate(0,x->x+1).limit(lista.size()).map(x->lista.get(x)).filter(x->!x.startsWith("A")).collect(Collectors.toCollection(ArrayList ::new));
+    }
+    public static List<Integer> por2MayorQue6(List<Integer> lista2){
+        return Stream.iterate(0,x->x+1).limit(lista2.size()).map(x->lista2.get(x)*2).filter(x->x>6).collect(Collectors.toCollection(ArrayList ::new));
+    }
+
 }
